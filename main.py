f0474d6c15bf54dec654b25049ae0f317f4949f1
wolfsoad <pmramaia.engmec@gmail.com>
2022-05-26 20:43:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,19 @@ URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/m
 
 # Write your code below this line 👇
 
+response = requests.get(URL)
+soup = BeautifulSoup(response.text, "html.parser")
+movies_data = soup.find_all(name="h3", class_="title")
+
+movies_titles = [movie.getText() for movie in movies_data]
+movies_titles.reverse()
+
+open("movies.txt", mode="w")
+with open("movies.txt", mode="w") as file:
+    for title in movies_titles:
+        file.write(f"{title}\n")
+
+
+
+
 
